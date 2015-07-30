dfce08b06c403e5b6ff1eaf64bcf701804d7d0fe
Rob Graham <altabyte@gmail.com>
2015-07-30 01:44:48 +0100

    Make a GeteBayOfficialTime eBay API request and display the response time on the home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
+require 'ebay_trading'
+require 'ebay_trading/request'
+
 class HomeController < ApplicationController
   def index
+    @ebay_username = ENV['EBAY_API_USERNAME_TT']
+    auth_token = EbayTrading.configuration.auth_token_for(@ebay_username)
+    request = EbayTrading::Request.new('GeteBayOfficialTime', auth_token)
+    @ebay_time = request.timestamp
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,16 @@
+require 'ebay_trading'
+
+EbayTrading.configure do |config|
+  config.environment = :production
+  config.ebay_site_id = 3 # ebay.co.uk
+  config.dev_id  = ENV['EBAY_API_DEV_ID']
+  config.app_id  = ENV['EBAY_API_APP_ID']
+  config.cert_id = ENV['EBAY_API_CERT_ID']
+
+  config.store_auth_token(ENV['EBAY_API_USERNAME_AR'], ENV['EBAY_API_AUTH_TOKEN_AR'])
+  config.store_auth_token(ENV['EBAY_API_USERNAME_TT'], ENV['EBAY_API_AUTH_TOKEN_TT'])
+end
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 

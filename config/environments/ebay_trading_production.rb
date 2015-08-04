555f26db41c34c24599880ee10adc34eece6c6ee
Rob Graham <altabyte@gmail.com>
2015-08-04 18:10:01 +0100

    Update EbayTrading configurations to ensure Money types are used for prices.

--- config/environments/ebay_trading_production.rb
+++ config/environments/ebay_trading_production.rb
@@ -1,11 +1,19 @@
 require 'ebay_trading'
 
 EbayTrading.configure do |config|
+
+  # http://developer.ebay.com/DevZone/XML/docs/ReleaseNotes.html
+  config.ebay_api_version = 931
+
   config.environment = :production
+
   config.ebay_site_id = 3 # ebay.co.uk
+
   config.dev_id  = ENV['EBAY_API_DEV_ID']
   config.app_id  = ENV['EBAY_API_APP_ID']
   config.cert_id = ENV['EBAY_API_CERT_ID']
+
+  config.price_type = :money
 
   config.store_auth_token(ENV['EBAY_API_USERNAME_AR'], ENV['EBAY_API_AUTH_TOKEN_AR'])
   config.store_auth_token(ENV['EBAY_API_USERNAME_TT'], ENV['EBAY_API_AUTH_TOKEN_TT'])

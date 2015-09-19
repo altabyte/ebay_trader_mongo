b6a094a167daf77a99530fae6966689cdca8c781
Rob Graham <altabyte@gmail.com>
2015-09-19 23:15:18 +0100

    Change the application name in the session_store and sidekiq initializers.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0', namespace: "ebay_trading_ark_#{Rails.env}" }
+  config.redis = { url: 'redis://localhost:6379/0', namespace: "ebay_trader_mongo_#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0', namespace: "ebay_trading_ark_#{Rails.env}" }
+  config.redis = { url: 'redis://localhost:6379/0', namespace: "ebay_trader_mongo_#{Rails.env}" }
 end
